590b34e07f8185cee18fccd64d157b2e8f39d5fc
savasto <savastova.olga@gmail.com>
2023-11-03 11:44:25 +0100

    Version 0.2 of MyFlowerShop activity logs corrected

--- demo/src/main/java/com/example/demo/restController/PlantRestController.java
+++ demo/src/main/java/com/example/demo/restController/PlantRestController.java
@@ -1,10 +1,16 @@
 package com.example.demo.restController;
 
 
+import com.example.demo.model.ActivityLog;
 import com.example.demo.model.Plant;
-//import com.example.demo.service.ActivityLogService;
+import com.example.demo.service.ActivityLogService;
+import com.example.demo.service.ActivityLogService;
 import com.example.demo.service.PlantService;
+import com.example.demo.utilities.Utilities;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,16 +23,35 @@ public class PlantRestController {
     @Autowired
     PlantService plantService;
 
-   /* @Autowired
-    ActivityLogService activityLogService;*/
+    @Autowired
+    ActivityLogService activityLogService;
 
     @GetMapping("/getAllPlants")
-    public ArrayList<Plant> allPlants(Model container) {
-        ArrayList<Plant> plantsFromService = plantService.getAllPlants();
-        return plantService.getAllPlants();
-    }
 
-    @GetMapping("/getPlantById/{id}")
+    public ResponseEntity<Iterable<Plant>> getAllPlants(HttpServletRequest request) {
+
+        // query to service to get all books
+        ArrayList<Plant> plantsFromService = plantService.getAllPlants();
+        // call Utilities to create a log
+        ActivityLog activityLog = Utilities.createLog(request, "getAllPlants",
+                "plants", "processing", "/api/v1/plants/getAllPlants", "GET");
+        HttpHeaders headers = Utilities.createHeader(activityLog);
+        //Timestamp timestamp2 = new Timestamp(System.currentTimeMillis());
+        //System.out.println("Lapsed time: " + ( timestamp2.getNanos() - timestamp.getNanos()));
+
+        if (plantsFromService != null) {
+            activityLog.setStatus("success");
+            activityLogService.addActivityLog(activityLog);
+            headers.add("status", "success");
+            return ResponseEntity.accepted().headers(headers).body(plantsFromService);
+        } else {
+            activityLog.setStatus("fail");
+            activityLogService.addActivityLog(activityLog);
+            headers.add("status", "fail");
+            return ResponseEntity.internalServerError().headers(headers).body(plantsFromService);
+        }
+
+    /*@GetMapping("/getPlantById/{id}")
     public Plant getPlantById(@PathVariable String id){
         Plant plant=null;
         plant=plantService.findPlantById(id);
@@ -36,7 +61,7 @@ public class PlantRestController {
     public void deleteAllPlants()
     {
         plantService.deleteAllPlants();
-    }
+    }*/
 
     /*@DeleteMapping("/deletePlantById/{id}"){
         public String deletePlantById(@PathVariable String id){
@@ -54,4 +79,4 @@ public class PlantRestController {
     }*/
 
 
-}
+    }}
--- demo/src/main/java/com/example/demo/service/ActivityLogService.java
+++ demo/src/main/java/com/example/demo/service/ActivityLogService.java
@@ -1,27 +1,29 @@
-//package com.example.demo.service;
-//
-//import com.example.demo.model.ActivityLog;
-//import org.springframework.beans.factory.annotation.Autowired;
-//
-//import java.util.ArrayList;
-//
-//public class ActivityLogService {
-//    static ArrayList<ActivityLog> activityLogs = new ArrayList<>();
-//
-//
-//
-//    // return plants to controller
-//    // get plants form list static from class and return as signature
-//    public ArrayList<ActivityLog> getAllLogs (){
-//        return activityLogs;
-//    }
-//
-//
-//    public boolean addActivityLog (ActivityLog activityLog){
-//
-//        boolean resultAdding = activityLogs.add(activityLog);
-//        //System.out.println(activityLogs);
-//
-//        return resultAdding;
-//    }
-//}
+package com.example.demo.service;
+
+
+
+import com.example.demo.model.ActivityLog;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+@Service
+public class ActivityLogService {
+    static ArrayList<ActivityLog> activityLogs = new ArrayList<>();
+
+
+
+    // return plants to controller
+    // get plants form list static from class and return as signature
+    public ArrayList<ActivityLog> getAllLogs (){
+        return activityLogs;
+    }
+
+
+    public boolean addActivityLog (ActivityLog activityLog){
+
+        boolean resultAdding = activityLogs.add(activityLog);
+        //System.out.println(activityLogs);
+
+        return resultAdding;
+    }
+}
